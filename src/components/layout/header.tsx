7c0de4fe365a44c7c559f33dc36b83329cc78e71
David Anaton <davidanaton@google.com>
2023-02-24 15:36:39 +0200

    Designed logo width

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,15 +6,17 @@ import Link from "next/link";
 export default function Header() {
   const { data: session } = useSession();
   return <div className={styles.header}>
-    <Link href='/'>
-      <Image 
-        src="/dvnt.svg"
-        width={100}
-        height={30}
-        alt="DVNT logo"
-        className={styles.headerLogo}
-      />
-    </Link>
+    <div className={styles.logoContainer}>
+      <Link href='/'>
+        <Image 
+          src="/dvnt.svg"
+          width={100}
+          height={30}
+          alt="DVNT logo"
+          className={styles.logo}
+        />
+      </Link>
+    </div>
     {session ?
       <Image 
         src={session.user?.image ?? ''}
